a8fff91df263bdad4be2ca95b38c5776d315754b
Vilberto02 <albertojulcapatricio@gmail.com>
2024-07-29 11:37:23 -0500

    password generate

--- 30-days-30-projects/04-Password_Generator/main.js
+++ 30-days-30-projects/04-Password_Generator/main.js
@@ -6,74 +6,70 @@ const numberBtn = document.getElementById("numbers-btn");
 const symbolBtn = document.getElementById("symbols-btn");
 const generateBtn = document.querySelector(".generate");
 const generateInput = document.getElementById("input-generate");
+const copyBtn = document.querySelector(".copy");
 
 let options = {
-  uppercase: false,
-  numbers: false,
-  symbols: false,
+  uppercase: "A B C D E F G H I J K L M N O P Q R S T U V W X Z",
+  lowercase: "a b c d e f g h i j k l m n o p q r s t u v w x z",
+  numbers: "0 1 2 3 4 5 6 7 8 9",
+  symbols: "/ * - + . , { } [ ] $ % & ( ) = ! # _ ^",
 };
 
-const symbolsValue = "/ * - + . , { } [ ] $ % & ( ) = ! #".split(" ");
-const uppercaseValue =
-  "A B C D E F G H I J K L M N O P Q R S T U V W X Z".split(" ");
-const lowercaseValue =
-  "a b c d e f g h i j k l m n o p q r s t u v w x z".split(" ");
-const numbersValue = "0 1 2 3 4 5 6 7 8 9".split(" ");
+let configuration = {
+  characters: parseInt(inputBox.value),
+  uppercase: true,
+  lowercase: true,
+  numbers: true,
+  symbols: true,
+};
 
 lessBtn.addEventListener("click", () => {
-  start(inputBox.value, true);
+  if (configuration.characters > 4) {
+    configuration.characters--;
+    //console.log(configuration.characters);
+    inputBox.value = configuration.characters;
+  } else {
+    alert("Número mínimo de caracteres 4");
+  }
 });
 
 moreBtn.addEventListener("click", () => {
-  start(inputBox.value, false);
+  configuration.characters++;
+  inputBox.value = configuration.characters;
+  //console.log(configuration.characters);
 });
-
-function start(input, substract) {
-  let value = parseInt(input);
-  if (value > 4 && substract) {
-    value = value - 1;
-    console.log(value);
-    inputBox.value = value;
-  } else if (!substract) {
-    value = value + 1;
-    console.log(value);
-    inputBox.value = value;
-  } else {
-    alert("Numero minimo de caracteres 4");
-  }
-}
-/*
-uppercaseBtn.addEventListener("click", () => {
-  /*const value = uppercaseBtn.innerText.toLowerCase();
-  if (value == "si") {
-    uppercaseBtn.innerHTML = "No";
-    uppercase = true;
-  } else {
-    uppercaseBtn.innerHTML = "Si";
-    uppercase = false;
-  }
-  console.log(uppercase);*/
-/*options(uppercaseBtn, uppercase);
-});*/
-/*
-numberBtn.addEventListener("click", () => {
-  options(numberBtn, numbers);
-});
-
-symbolBtn.addEventListener("click", () => {
-  options(symbolBtn, symbols);
-});*/
 
 [uppercaseBtn, numberBtn, symbolBtn].forEach((btn) => {
   btn.addEventListener("click", () => {
+    //console.log(btn.id);
     toggleOptions(btn);
   });
 });
 
 function toggleOptions(btn) {
-  const option = btn.getAttribute("data-option");
-  options[option] = !options[option];
-  btn.innerText = options[option] ? "No" : "Si";
+  btn.classList.toggle("false");
+  btn.firstElementChild.classList.toggle("bx-check");
+  btn.firstElementChild.classList.toggle("bx-x");
+
+  switch (btn.id) {
+    case "uppercase-btn":
+      configuration.uppercase = !configuration.uppercase;
+      break;
+    case "symbols-btn":
+      configuration.symbols = !configuration.symbols;
+      break;
+    case "numbers-btn":
+      configuration.numbers = !configuration.numbers;
+      break;
+  }
+  console.log(
+    "uppercase: " +
+      configuration.uppercase +
+      "\nnumbers: " +
+      configuration.numbers +
+      "\nsymbols: " +
+      configuration.symbols
+  );
 }
 
 generateBtn.addEventListener("click", () => {
@@ -81,18 +77,40 @@ generateBtn.addEventListener("click", () => {
 });
 
 function generatePassword() {
-  const length = parseInt(inputBox.value);
-  let characters = [...lowercaseValue];
-
-  if (options.uppercase) characters = characters.concat(uppercaseValue);
-  if (options.numbers) characters = characters.concat(numbersValue);
-  if (options.symbols) characters = characters.concat(symbolsValue);
-
+  let charactersTemp = "";
   let password = "";
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    password += characters[randomIndex];
-  }
 
+  for (property in configuration) {
+    if (configuration[property] == true) {
+      charactersTemp += options[property] + " ";
+    }
+  }
+  charactersTemp = charactersTemp.trim();
+  charactersTemp = charactersTemp.split(" ");
+
+  for (let i = 0; i < configuration.characters; i++) {
+    password =
+      password +
+      charactersTemp[Math.floor(Math.random() * charactersTemp.length)];
+  }
   generateInput.value = password;
+  console.log(password);
 }
+
+copyBtn.addEventListener("click", () => {
+  copyPassword();
+});
+
+function copyPassword() {
+  generateInput.select();
+  navigator.clipboard
+    .writeText(generateInput.value)
+    .then(() => {
+      alert("Texto copiado al portapapeles!");
+    })
+    .catch((err) => {
+      console.error("No se pudo copiar el texto: ", err);
+    });
+}
+
+generatePassword();
