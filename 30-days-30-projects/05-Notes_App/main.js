89bfe43f6e6cd4890cb08424c4e358ec6265ebb2
Vilberto02 <albertojulcapatricio@gmail.com>
2024-07-30 17:29:05 -0500

    Finish note app

--- 30-days-30-projects/05-Notes_App/main.js
+++ 30-days-30-projects/05-Notes_App/main.js
@@ -1,9 +1,12 @@
 const addBtn = document.querySelector(".add__box");
 const popup = document.querySelector(".popup__box");
 const closePopup = document.querySelector(".header__popup i");
+const titleHeader = document.querySelector(".header__popup h3");
 const submitBtn = document.querySelector(".add__btn");
 const titleInput = document.getElementById("title-input");
 const descriptionInput = document.getElementById("description-textarea");
+const editNoteBtn = document.querySelector(".edit");
+const deleteNoteBtn = document.querySelector(".delete");
 
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 let dateObj = new Date();
@@ -21,10 +24,12 @@ const months = [
   "Noviembre",
   "Diciembre",
 ];
+let isUpdate = false;
+let updateId;
 
 function showNotes() {
   document.querySelectorAll(".note").forEach((note) => note.remove());
-  notes.forEach((note) => {
+  notes.forEach((note, index) => {
     let liTag = `
       <li class="note">
         <div class="details">
@@ -36,7 +41,11 @@ function showNotes() {
         <div class="bottom__content">
           <p>${note.date}</p>
           <div class="settings">
-            <i class="bx bx-dots-vertical-rounded"></i>
+            <i onclick="showMenu(this)" class="bx bx-dots-vertical-rounded"></i>
+            <ul class="menu">
+              <li onclick="editNote(${index}, '${note.title}', '${note.description}')"><i class="bx bx-pencil"></i>Editar</li>
+              <li onclick="deleteNote(${index})" ><i class="bx bxs-trash"></i>Eliminar</li>
+            </ul>
           </div>
         </div>
       </li>
@@ -46,11 +55,49 @@ function showNotes() {
 }
 showNotes();
 
+function showMenu(element) {
+  element.parentElement.classList.add("show");
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName != "I" || e.target != element) {
+      element.parentElement.classList.remove("show");
+    }
+  });
+}
+
+function deleteNote(noteId) {
+  console.log(noteId);
+  let confirmationDelete = confirm("Quieres eliminar esta nota?");
+  if (!confirmationDelete) {
+    return;
+  }
+  notes.splice(noteId, 1);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  showNotes();
+}
+
+function editNote(noteId, title, description) {
+  console.log(noteId);
+  addBtn.click();
+  //console.log(notes[noteId].title);
+  titleInput.value = title;
+  descriptionInput.value = description;
+  updateId = noteId;
+  isUpdate = true;
+  submitBtn.innerHTML = "Update Note";
+  titleHeader.innerHTML = "Update a Note";
+}
+
 addBtn.addEventListener("click", () => {
   popup.classList.add("show");
+  titleInput.focus();
 });
 
 closePopup.addEventListener("click", () => {
+  titleInput.value = "";
+  descriptionInput.value = "";
+  isUpdate = false;
+  submitBtn.innerHTML = "Add Note";
+  titleHeader.innerHTML = "Add a new Note";
   popup.classList.remove("show");
 });
 
@@ -67,7 +114,13 @@ submitBtn.addEventListener("click", (e) => {
       description: descriptionInput.value,
       date: `${month} ${day}, ${year}`,
     };
-    notes.push(noteInfo);
+
+    if (!isUpdate) {
+      notes.push(noteInfo);
+    } else {
+      isUpdate = false;
+      notes[updateId] = noteInfo;
+    }
     localStorage.setItem("notes", JSON.stringify(notes));
     titleInput.value = "";
     descriptionInput.value = "";
