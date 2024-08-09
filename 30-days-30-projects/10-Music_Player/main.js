658a777a5bb7072704ca58b82f406ed9134aa6d1
Vilberto02 <albertojulcapatricio@gmail.com>
2024-08-09 01:09:41 -0500

    music player done

--- 30-days-30-projects/10-Music_Player/main.js
+++ 30-days-30-projects/10-Music_Player/main.js
@@ -2,13 +2,13 @@ const image = document.getElementById("cover"),
   background = document.getElementById("bg-img"),
   title = document.getElementById("song__title"),
   artist = document.getElementById("song__artist"),
-  currentTimeEl = document.getElementById("curent-time"),
+  currentTimeEl = document.getElementById("current-time"),
   durationEl = document.getElementById("duration"),
   progress = document.getElementById("progress"),
   playerProgress = document.getElementById("progress-bar"),
   prevBtn = document.getElementById("prev"),
   playBtn = document.getElementById("play"),
-  nextBtn = document.getElementById("mnxt");
+  nextBtn = document.getElementById("next");
 
 const music = new Audio();
 
@@ -66,5 +66,49 @@ function loadMusic(song) {
   background.src = song.cover;
 }
 
+function changeMusic(direction) {
+  musicIndex = (musicIndex + direction + songs.length) % songs.length;
+  loadMusic(songs[musicIndex]);
+  playMusic();
+}
+/*
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+  return `${String(minutes).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+}*/
+
+function updateProgressBar() {
+  const { duration, currentTime } = music;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+
+  const formatTime = (time) => String(Math.floor(time)).padStart(2, "0");
+
+  durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(
+    duration % 60
+  )}`;
+  console.log(durationEl);
+  currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(
+    currentTime % 60
+  )}`;
+  console.log(durationEl);
+  /*
+  durationEl.textContent = formatTime(duration);
+  currentTimeEl.textContent = formatTime(currentTime);*/
+}
+
+function setProgressBar(e) {
+  const width = playerProgress.clientWidth;
+  const clickX = e.clientX - playerProgress.getBoundingClientRect().left;
+  music.currentTime = (clickX / width) * music.duration;
+}
+
 playBtn.addEventListener("click", togglePlay);
-loadMusic(songs[1]);
+prevBtn.addEventListener("click", () => changeMusic(-1));
+nextBtn.addEventListener("click", () => changeMusic(1));
+music.addEventListener("ended", () => changeMusic(1));
+music.addEventListener("timeupdate", updateProgressBar);
+playerProgress.addEventListener("click", setProgressBar);
+
+loadMusic(songs[musicIndex]);
